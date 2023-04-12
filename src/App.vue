56935a27c29af2b76cb6e89390ff55fe106eb650
LookasWasTaken~ <piacen.luca@gmail.com>
2023-04-12 16:32:57 +0200

    CHANGE text interpolation

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <script>
+import { VueElement } from 'vue';
+
 export default {
   data(){
     return{
-
+      messageHello: "Hello Vue.js!",
+      messageBy: "made with 2minutes of time by Lookas"
     }
   }
 }
 </script>
 
 <template>
-<h1>Hello Vue.js!</h1>
-<h2>made with 2minutes of time by Lookas</h2>
+<h1>{{messageHello}}</h1>
+<h2>{{messageBy}}</h2>
 </template>
 
 <style>
